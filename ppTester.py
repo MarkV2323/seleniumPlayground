0d927f4145b2d590899ba54b2f4cb938a7e1e5d3
MarkV2323 <41304553+MarkV2323@users.noreply.github.com>
2022-06-20 08:21:30 -0700

    Added Constant varibles and cleaned pipeline

--- ppTester.py
+++ ppTester.py
@@ -3,33 +3,33 @@ import commonUtils
 from selenium.webdriver.common.by import By
 import time
 
-# Constants & useful variables
+# Constants variables
 URL_ELEMENT_XPATH = "//a[@class='rel-link']"
-start_page = sys.argv[1]
+IMPLICIT_WAIT_TIME = 2
+SCROLL_WAIT_TIME = 0.5
+START_PAGE = sys.argv[1]
+TEXT_OUTPUT = "out_data.txt"
+IMAGE_DIRECTORY = "images/"
 
+"""
+Program Pipeline
+"""
 # time program
 start_time = time.time()
-
 # Starts the driver with configured options and goes to start_page.
-driver = commonUtils.build_driver(2, True)
-driver.get(start_page)
+driver = commonUtils.build_driver(IMPLICIT_WAIT_TIME, True)
+driver.get(START_PAGE)
 print(f"Driver is now at start_page!")
-
 # Scrolling to bottom to find all possible elements.
-commonUtils.scroll_till_bottom(driver, 0.5)
-
+commonUtils.scroll_till_bottom(driver, SCROLL_WAIT_TIME)
 # Begin finding URLs to content galleries, then compile URLs into a list.
 galleries_to_vist = commonUtils.get_href_from_page(driver, By.XPATH, URL_ELEMENT_XPATH)
-
 # vist links one by one, save image URLs to list
 image_urls = commonUtils.get_hrefs_from_pages(driver, By.XPATH, URL_ELEMENT_XPATH, galleries_to_vist)
-
 # output to a text file
-commonUtils.write_text_file(image_urls, "out_data.txt")
-
+commonUtils.write_text_file(image_urls, TEXT_OUTPUT)
 # download images from text file.
-commonUtils.save_images_from_file("out_data.txt", "images/")
-
+commonUtils.save_images_from_file(TEXT_OUTPUT, IMAGE_DIRECTORY)
 # output total time for program to run.
 print(f"Program completed in {time.time() - start_time} seconds.")
 driver.close()
